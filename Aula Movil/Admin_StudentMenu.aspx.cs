6a17c486e01bc328afac8036e4e4cb15cd7ebb34
Blacksolid64 <memolopez8964@gmail.com>
2021-11-12 01:31:58 -0600

    Changes to the student ops

--- Aula Movil/Admin_StudentMenu.aspx.cs
+++ Aula Movil/Admin_StudentMenu.aspx.cs
@@ -24,34 +24,53 @@ namespace Aula_Movil
             GR_Students.DataBind();
         }
 
+        protected void GrCourses_OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GR_Students.EditIndex = e.NewEditIndex;
+            this.verEstudiantes();
+        }
+
+        protected void GrCourses_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GR_Students.EditIndex = -1;
+            this.verEstudiantes();
+        }
+
+
         protected void agregarEstdiantes(object sender, EventArgs e)
         {
             string apiURL = Application["apiURL"].ToString() + "nuevoAlumno/";
-            /* string ced = GridView1.Rows[i].FindControl("cedula"); //Probablemente malo
-             string nom = GridView1.Rows[i].FindControl("nombre");
-             string cor = GridView1.Rows[i].FindControl("correo");
-             string con = GridView1.Rows[i].FindControl("contraseña");
-             string ape = GridView1.Rows[i].FindControl("apellido");
-             string gra = GridView1.Rows[i].FindControl("grado");
-             apiURL = apiURL + ced + "/" + nom + "/" + cor + "/" + con + "/" + ape + "/" + gra;
-             APICaller apiCaller = new APICaller();
-             string apiResponse = apiCaller.RequestAPIData(apiURL);*/
+            string ced = txt_nuevaCedula.Text;
+            string nom = txt_nuevoNombre.Text;
+            string cor = txt_nuevoCorreo.Text;
+            string con = txt_nuevaContrasenna.Text;
+            string ape = txt_nuevoApellido.Text;
+            apiURL = apiURL + ced + "/" + nom + "/" + cor + "/" + con + "/" + ape;
+            APICaller apiCaller = new APICaller();
+            string apiResponse = apiCaller.RequestAPIData(apiURL);
+            txt_nuevaCedula.Text = "";
+            txt_nuevoNombre.Text = "";
+            txt_nuevoCorreo.Text = "";
+            txt_nuevaContrasenna.Text = "";
+            txt_nuevoApellido.Text = "";
+            this.verEstudiantes();
         }
 
-        protected void editarEstdiantes(object sender, GridViewEditEventArgs e)
+        protected void editarEstdiantes(object sender, GridViewUpdateEventArgs e)
         {
+            GridViewRow row = GR_Students.Rows[e.RowIndex];
             string apiURL = Application["apiURL"].ToString() + "updateAlumno/";
-            /* string nomv = GridView1.Rows[i].FindControl("nombre"); //Agarrar item viejo
-             string apev = GridView1.Rows[i].FindControl("apellido"); //Agarrar item viejo
-             string ced = GridView1.Rows[i].FindControl("cedula"); //Probablemente malo
-             string nom = GridView1.Rows[i].FindControl("nombre");
-             string cor = GridView1.Rows[i].FindControl("correo");
-             string con = GridView1.Rows[i].FindControl("contraseña");
-             string ape = GridView1.Rows[i].FindControl("apellido");
-             string gra = GridView1.Rows[i].FindControl("grado");
-             apiURL = apiURL + nomv + "/" + apev+ "/" + ced + "/" + nom + "/" + cor + "/" + con + "/" + ape + "/" + gra;
-             APICaller apiCaller = new APICaller();
-             string apiResponse = apiCaller.RequestAPIData(apiURL); */
+            string cedulaOriginal = (row.FindControl("lbl_OrigCed") as Label).Text;
+            string cedula = (row.FindControl("txt_cedula") as TextBox).Text;
+            string nombre = (row.FindControl("txt_nombre") as TextBox).Text;
+            string apellido = (row.FindControl("txt_Apellido") as TextBox).Text;
+            string correo = (row.FindControl("txt_correo") as TextBox).Text;
+            string contrasenna = (row.FindControl("txt_contrasenna") as TextBox).Text;
+            apiURL = apiURL + cedulaOriginal + "/" + cedula + "/" + nombre + "/" + correo + "/" + contrasenna + "/" + apellido;
+            APICaller apiCaller = new APICaller();
+            string apiResponse = apiCaller.RequestAPIData(apiURL);
+            GR_Students.EditIndex = -1;
+            this.verEstudiantes();
         }
 
         protected void eliminarEstdiantes(object sender, GridViewEditEventArgs e)
